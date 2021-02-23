44169a1333b796b9ba6b2a40627db858fd944ce8
Marvin Fretter <marvin.fretter@gmail.com>
2021-02-23 11:46:42 +0100

    improved scripts

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,15 +1,16 @@
+
+
+
+
 // Get the modal
 let modal = document.getElementsByClassName("modal")[0];
-
-// Get the button that opens the modal
-let btn = document.getElementsByClassName("showcase-content__img")[0];
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
+// Get main content of whole page
 let main = document.getElementsByTagName("main")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+
+// When the user clicks the img, open the modal 
+function openModal(imgSrc){
+  let modalImg = document.getElementsByClassName("modal-content__modal-img")[0];
+  modalImg.src = imgSrc;
   modal.style.visibility = "visible";
   modal.style.opacity = 1;
   modal.style.width = "100%";
@@ -19,13 +20,12 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+function closeModal() {
   main.classList.remove("blur-2");
   modal.style.visibility = "hidden";
   modal.style.opacity = 0;
   modal.style.width = "33%";
   modal.style.height = "25%";
-  // main.classList.remove("scale-down");
 }
 
 // When the user clicks anywhere outside of the modal, close it
