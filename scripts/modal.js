b332acb0f8d9a15a3d2c8cd6023e737d05dd8e92
Marvin Fretter <marvin.fretter@gmail.com>
2021-02-24 19:56:22 +0100

    edited illus

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,15 +1,13 @@
-
-
-
-
 // Get the modal
 let modal = document.getElementsByClassName("modal")[0];
 // Get main content of whole page
 let main = document.getElementsByTagName("main")[0];
 
-// When the user clicks the img, open the modal 
-function openModal(project, imgSrc){
-  const modalImg = document.getElementsByClassName("modal-content__modal-img")[0];
+// When the user clicks the img, open the modal
+function openModal(project, imgSrc) {
+  const modalImg = document.getElementsByClassName(
+    "modal-content__modal-img"
+  )[0];
   modalImg.src = imgSrc;
   setText(project);
   modal.style.visibility = "visible";
@@ -29,18 +27,21 @@ function closeModal() {
   modal.style.height = "25%";
 }
 
-function setText(project){
+function setText(project) {
   const textEl = document.getElementsByClassName("modal-content__text")[0];
-  if(project === 'shopme'){
-    textEl.innerHTML = "This is shopme. I've designed and coded this app to help people out when they are struggling with their groceries. It was fun. To be continued!";
+  if (project === "shopme") {
+    textEl.innerHTML =
+      "This is shopme. I've designed and coded this app to help people out when they are struggling with their groceries. It was fun. To be continued!";
   }
-  if(project === 'wap'){
-    textEl.innerHTML = "This is Wap. I've designed and coded this app to help people find a nice spot in nature. It's like google maps but for hipsters and nature lovers. Enjoy!";
+  if (project === "wap") {
+    textEl.innerHTML =
+      "This is Wap. I've designed and coded this app to help people find a nice spot in nature. It's like google maps but for hipsters and nature lovers. Enjoy!";
   }
-  if(project === 'deep'){
-    textEl.innerHTML = "This is deep. I've designed this webshop for bass enthusiast. The goal is to put only a few, high-class and carefully selected basses in the spotlight. The shop is supposed to be less loaded then average instrument retailers to help the user focus on what matters: a good bass.";
+  if (project === "deep") {
+    textEl.innerHTML =
+      "This is deep. I've designed this webshop for bass enthusiast. The goal is to put only a few, high-class and carefully selected basses in the spotlight. The shop is supposed to be less loaded then average instrument retailers to help the user focus on what matters: a good bass.";
   }
-  if(project === 'mail'){
+  if (project === "mail") {
     const form = document.getElementsByTagName("form");
     console.log(Array.from(form));
     textEl.innerHTML = form;
@@ -48,12 +49,12 @@ function setText(project){
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.visibility = "hidden";
-  modal.style.opacity = 0;
-  modal.style.width = "33%";
-  modal.style.height = "25%";
-  main.classList.remove("blur-2");
+    modal.style.opacity = 0;
+    modal.style.width = "33%";
+    modal.style.height = "25%";
+    main.classList.remove("blur-2");
   }
-}
+};
--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,10 +1,16 @@
 const heroImageWrapper = document.getElementsByClassName(
   "hero__img-wrapper"
 )[0];
+const heroTextWrapper = document.getElementsByClassName(
+  "hero__text-wrapper"
+)[0];
 document.addEventListener("scroll", function () {
-  let shrink = 1 - this.documentElement.scrollTop / 500;
-  let grow = this.documentElement.scrollTop / 100;
-  heroImageWrapper.style.transform = "scale(" + shrink + ")";
-  // heroImageWrapper.style.filter = 'blur(' + grow + 'px)';
-  // heroImageWrapper.style.top = grow + 'px';
+  if (this.documentElement.scrollTop < 500) {
+    let shrink = 1 - this.documentElement.scrollTop / 500;
+    let grow = this.documentElement.scrollTop / 100;
+    heroImageWrapper.style.transform = "scale(" + shrink + ")";
+    heroTextWrapper.style.transform = "scale(" + shrink + ")";
+    // heroImageWrapper.style.filter = 'blur(' + grow + 'px)';
+    // heroImageWrapper.style.top = grow + 'px';
+  }
 });
