0328a1518850a5a0a258c4e8242fd2aa4f0849d9
Marvin Fretter <marvin.fretter@gmail.com>
2021-03-02 12:05:37 +0100

    .

--- scripts/form.js
+++ scripts/form.js
@@ -5,18 +5,20 @@
 //   let myForm = document.querySelector('contact-form');
 //   let formData = new FormData(myForm)
 //   console.log(formData);
-  // fetch('/', {
-  //   method: 'POST',
-  //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //   body: new URLSearchParams(formData).toString()
-  // }).then(() => console.log('Form successfully submitted')).catch((error) =>
-  //   alert(error))
+// fetch('/', {
+//   method: 'POST',
+//   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//   body: new URLSearchParams(formData).toString()
+// }).then(() => console.log('Form successfully submitted')).catch((error) =>
+//   alert(error))
 // }
 
-
-function sendForm(e,form) {
-  fetch(form.action,{method:'post', body: new FormData(form)});
-
-  console.log('We send post asynchronously (AJAX)');
+async function sendForm(e, form) {
+  try{
   e.preventDefault();
+  console.log(new FormData(form));
+  let res = await fetch(form.action, { method: "post", body: new FormData(form) });
+  console.log(res.json())
+  console.log("We send post asynchronously (AJAX)");
+  } catch(err){console.log(err)}
 }
