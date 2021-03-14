789a7204d7c3589968b8250406cfe8722045d891
Marvin Fretter <marvin.fretter@gmail.com>
2021-03-14 17:39:12 +0100

    ajax contact form prototype

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,25 @@
 const form = document.querySelector("form");
+const submitButton = document.querySelector(".contact-form__submit-button");
+const letterIllu = document.querySelector(".contact-form__letter-illu");
+const letterIlluWrapper = document.querySelector(
+  ".contact-form__letter-illu-wrapper"
+);
 
 const handleSubmit = (e) => {
   e.preventDefault();
-
+  letterIlluWrapper.classList.add("send");
   const formData = new FormData(form);
 
-  fetch("/", {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
     .then(function (response) {
       if (response.ok) {
-        return console.log("Form successfully submitted");
+        console.log("Form successfully submitted");
+        letterIlluWrapper.innerHTML="Success!";
+        return
       }
       return Promise.reject(response);
     })
@@ -21,16 +28,33 @@ const handleSubmit = (e) => {
     })
     .catch(function (error) {
       console.warn(error);
+      letterIlluWrapper.innerHTML="Failed &times;";
+    })
+    .finally(() => {
+        
+      setTimeout(function(){letterIlluWrapper.classList.remove("send")},400);
     });
 };
 
-form.addEventListener("submit", handleSubmit);
+const flipImg = (e) => {
+  e.preventDefault;
 
-// let myForm = document.getElementById('pizzaOrder');
-//   let formData = new FormData(myForm)
-//   fetch('/', {
-//     method: 'POST',
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: new URLSearchParams(formData).toString()
-//   }).then(() => console.log('Form successfully submitted')).catch((error) =>
-//     alert(error))
+  letterIllu.classList.add("flip");
+
+  setTimeout(function () {
+    if (e.type === "mouseenter") {
+      letterIllu.src = "../img/illu/Letter_front.png";
+    }
+    if (e.type === "mouseleave") {
+      letterIllu.src = "../img/illu/Letter_back.png";
+    }
+  }, 150);
+
+  setTimeout(function () {
+    letterIllu.classList.remove("flip");
+  }, 300);
+};
+
+form.addEventListener("submit", handleSubmit);
+submitButton.addEventListener("mouseenter", flipImg);
+submitButton.addEventListener("mouseleave", flipImg);
