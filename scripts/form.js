e6c05df3d5e601695d57fc3459670223d73588fe
Marvin Fretter <marvin.fretter@gmail.com>
2021-03-15 15:23:01 +0100

    check required fields

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+
 const submitButton = document.querySelector(".contact-form__submit-button");
 const letterIllu = document.querySelector(".contact-form__letter-illu");
 const letterIlluWrapper = document.querySelector(
@@ -7,35 +8,40 @@ const letterIlluWrapper = document.querySelector(
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  letterIlluWrapper.classList.add("send");
-  const formData = new FormData(form);
+  if (checkRequiredFields()) {
+    letterIlluWrapper.classList.add("send");
+    const formData = new FormData(form);
 
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(function (response) {
-      if (response.ok) {
-        console.log("Form successfully submitted");
-        letterIlluWrapper.innerHTML="Success!";
-        return
-      }
-      return Promise.reject(response);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
     })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.warn(error);
-      letterIlluWrapper.innerHTML="Failed &times;";
-    })
-    .finally(() => {
-        
-      setTimeout(function(){letterIlluWrapper.classList.remove("send")},400);
-    });
+      .then(function (response) {
+        if (response.ok) {
+          console.log("Form successfully submitted");
+          letterIlluWrapper.innerHTML = "Success!";
+          return;
+        }
+        return Promise.reject(response);
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.warn(error);
+        setTimeout(function () {
+          letterIlluWrapper.innerHTML = "Failed &times;";
+        }, 400);
+      })
+      .finally(() => {
+        setTimeout(function () {
+          letterIlluWrapper.classList.remove("send");
+        }, 400);
+      });
+  }
+  if (!checkRequiredFields()) console.log("fields requoired");
 };
-
 const flipImg = (e) => {
   e.preventDefault;
 
@@ -58,3 +64,12 @@ const flipImg = (e) => {
 form.addEventListener("submit", handleSubmit);
 submitButton.addEventListener("mouseenter", flipImg);
 submitButton.addEventListener("mouseleave", flipImg);
+
+function checkRequiredFields() {
+  let fullname = document.querySelector("#fullname").value.replace(/\s/g, '');
+  let email = document.querySelector("#email").value.replace(/\s/g, '');
+  let subject = document.querySelector("#subject").value.replace(/\s/g, '');
+  let message = document.querySelector("#message").value.replace(/\s/g, '');
+  if (fullname && email && subject && message) return true;
+  return false;
+}
